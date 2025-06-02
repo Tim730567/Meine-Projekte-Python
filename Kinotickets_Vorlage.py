a0e8ce02b337bfe063c492e66c1a9fe6eda8ee68
Tim730567 <t.schneider64@icloud.com>
2025-06-02 18:33:16 +0200

    Add files via upload

---
+++ Kinotickets_Vorlage.py
@@ -0,0 +1,9 @@
+alter = int(input("Wie alt sind Sie?"))
+anzahl = int(input("Wieviele tickets möchten Sie haben?"))
+
+if alter > 65:
+    print(7.50 * anzahl)
+elif alter <= 18:
+    print(5 * anzahl)
+else:
+    print(10 * anzahl)
