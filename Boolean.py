4137d0f1e5cc4e8aafda6f430c0f16d6276df3b3
Tim730567 <t.schneider64@icloud.com>
2025-06-02 18:35:46 +0200

    Add files via upload

---
+++ Boolean.py
@@ -0,0 +1,11 @@
+streckeInKm = 3
+
+if streckeInKm < 3:
+    print("Lauf doch zu Fuß!")
+elif  streckeInKm == 3:
+    print("Puh die Strecke ist ganz schön lang.")
+    print("Nimm die Laufschuhe mit!")
+
+
+else:
+    print("Nimm doch hier das Fahrrad!")
---
+++ Währungsumrechner_+_Text.py
@@ -0,0 +1,5 @@
+euro =float(input("Wieviel Euro hast du?"))
+
+Baht = euro *  38.06
+
+print("Super! Deine" , euro , "Euro sind" , Baht , "Baht wert.")
